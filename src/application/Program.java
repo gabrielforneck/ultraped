8c3582c461e00153f9e195a667197ffecf7f0b95
Gabriel Schafer Forneck <gabrielforneck2004@gmail.com>
2024-05-12 22:54:53 -0300

    UCS-1 Remove importacoes nao usadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,10 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import menu.Menu;
 import menu.defaultoptions.CancelOption;
 import menu.defaultoptions.MenuOption;
-import supplier.Supplier;
-import supplier.crud.SupplierCrud;
 
 public class Program {
 
