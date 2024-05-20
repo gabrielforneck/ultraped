f0f19c4a387c5a7bb3ac98f96611f770bbbe73ea
Gabriel Schafer Forneck <gabrielforneck2004@gmail.com>
2024-05-20 02:03:02 -0300

    UCS-1 Remove importações não usadas

--- src/crud/CrudField.java
+++ src/crud/CrudField.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 import consoleinterface.nextaction.NextAction;
-import consoleinterface.table.IConsoleTable;
 import menu.options.interfaces.IMenuOption;
 import result.Result;
 
