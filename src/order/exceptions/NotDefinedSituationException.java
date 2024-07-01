8dd409b57469db53ff380605ad7b3f7c16e9a871
Gabriel Schafer Forneck <gabrielforneck2004@gmail.com>
2024-07-01 03:33:39 -0300

    UCS-4 Remove todos

--- src/order/exceptions/NotDefinedSituationException.java
+++ src/order/exceptions/NotDefinedSituationException.java
@@ -5,28 +5,23 @@ public class NotDefinedSituationException extends RuntimeException {
 	private static final long serialVersionUID = 6946460052180927469L;
 
 	public NotDefinedSituationException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotDefinedSituationException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotDefinedSituationException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotDefinedSituationException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotDefinedSituationException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
